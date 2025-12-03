6b2802d93e7463f88af57ca30076629eee13c710
Mauricio Luque Jiménez <72136756+mauricioluquejimenez@users.noreply.github.com>
2025-12-03 17:16:59 +0100

    Dictionary: cabecera e implementación de funciones básicas

--- include/dictionary.h
+++ include/dictionary.h
@@ -5,7 +5,6 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
-
 #include <string>
 #include <iostream>
 #include <set>
@@ -14,17 +13,57 @@
 using namespace std;
 
 class Dictionary {
-private:
+  private:
+    set<string> words;
 
+  public:
+    Dictionary();
+    ~Dictionary();
+    void clear();
+    unsigned int size() const;
+    bool empty() const;
+    bool exists(const string &val);
+    bool erase(const string &val);
+    friend istream &operator>>(istream &is, Dictionary &dic);
+    friend ostream &operator<<(ostream &os, const Dictionary &dic);
+    int getOcurrences(const char c) const;
+    int getTotalLetters() const;
+    vector<string> getWordsLength(int length);
 
-  set<string> words;
+    class iterator
+    {
+      private:
+        set<string>::iterator it;
 
-//A completar
+      public:
+        friend class Dictionary;
+        iterator();
+        bool operator==(const iterator &i) const;
+        bool operator!=(const iterator &i) const;
+        iterator &operator++();
+        const string &operator*();
+    };
 
+    class const_iterator
+    {
+      private:
+        set<string>::const_iterator it;
 
+      public:
+        friend class Dictionary;
+        const_iterator();
+        bool operator==(const const_iterator &i) const;
+        bool operator!=(const const_iterator &i) const;
+        const_iterator &operator++();
+        const string &operator*() const;
+    };
 
-
-
-
+    iterator find(const string &w);
+    pair<iterator, bool> insert(const string &val);
+    pair<iterator, iterator> range_prefix(const string &val);
+    iterator begin();
+    const_iterator begin() const;
+    iterator end();
+    const_iterator end() const;
 };
 #endif //DICTIONARY_H
--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -1,3 +1,136 @@
 #include "dictionary.h"
-//A completar
 
+Dictionary::Dictionary()
+{
+    this->words = set<string>();
+}
+
+Dictionary::~Dictionary()
+{
+    clear();
+}
+
+void Dictionary::clear()
+{
+    this->words.clear();
+}
+
+unsigned int Dictionary::size() const
+{
+    return this->words.size();
+}
+
+bool Dictionary::empty() const
+{
+    return this->words.empty();
+}
+
+bool Dictionary::exists(const string &val)
+{
+    return this->words.find(val) != this->words.end();
+}
+
+bool Dictionary::erase(const string &val)
+{
+    return this->words.erase(val);
+}
+
+int Dictionary::getOcurrences(const char c) const
+{
+
+}
+
+int Dictionary::getTotalLetters() const
+{
+
+}
+
+vector<string> Dictionary::getWordsLength(int length)
+{
+
+}
+
+Dictionary::iterator::iterator()
+{
+
+}
+
+bool Dictionary::iterator::operator==(const iterator &i) const
+{
+    this->it == i.it;
+}
+
+bool Dictionary::iterator::operator!=(const iterator &i) const
+{
+    this->it != i.it;
+}
+
+Dictionary::iterator &Dictionary::iterator::operator++()
+{
+    ++this->it;
+}
+
+const string &Dictionary::iterator::operator*()
+{
+    return *this->it;
+}
+
+Dictionary::const_iterator::const_iterator()
+{
+
+}
+
+bool Dictionary::const_iterator::operator==(const const_iterator &i) const
+{
+    this->it == i.it;
+}
+
+bool Dictionary::const_iterator::operator!=(const const_iterator &i) const
+{
+    this->it != i.it;
+}
+
+Dictionary::const_iterator &Dictionary::const_iterator::operator++()
+{
+    ++this->it;
+}
+
+const string &Dictionary::const_iterator::operator*() const
+{
+    return *this->it;
+}
+
+Dictionary::iterator Dictionary::find(const string &w)
+{
+
+}
+
+pair<Dictionary::iterator, bool> Dictionary::insert(const string &val)
+{
+
+}
+
+pair<Dictionary::iterator, Dictionary::iterator> Dictionary::range_prefix(const string &prefix)
+{
+
+}
+
+Dictionary::iterator Dictionary::begin()
+{
+
+}
+
+Dictionary::const_iterator Dictionary::begin() const
+{
+
+}
+
+Dictionary::iterator Dictionary::end()
+{
+
+}
+
+Dictionary::const_iterator Dictionary::end() const
+{
+
+}
