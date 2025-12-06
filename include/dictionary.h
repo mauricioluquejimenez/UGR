bd01bf8f08305f9084c6ef97825396b94372969d
Mauricio Luque Jiménez <72136756+mauricioluquejimenez@users.noreply.github.com>
2025-12-06 22:08:46 +0100

    Dictionary presuntamente  terminado

--- include/dictionary.h
+++ include/dictionary.h
@@ -8,8 +8,8 @@
 #include <string>
 #include <iostream>
 #include <set>
-
 #include <vector>
+
 using namespace std;
 
 class Dictionary {
@@ -17,52 +17,147 @@ class Dictionary {
     set<string> words;
 
   public:
-    Dictionary();
-    ~Dictionary();
-    void clear();
-    unsigned int size() const;
-    bool empty() const;
-    bool exists(const string &val);
-    bool erase(const string &val);
-    friend istream &operator>>(istream &is, Dictionary &dic);
-    friend ostream &operator<<(ostream &os, const Dictionary &dic);
-    int getOcurrences(const char c) const;
-    int getTotalLetters() const;
-    vector<string> getWordsLength(int length);
 
+    /**
+    * @brief Constructor por defecto. Crea un Dictionary vacío
+    */
+    Dictionary(){};
+
+    /**
+    * @brief Destructor
+    */
+    ~Dictionary(){};
+
+    /**
+    * @brief Limpia el Dictionary
+    * @post el diccionario queda con 0 palabras
+    */
+    void clear();
+
+    /**
+    * @brief Tamaño del diccionario
+    * @return Número de palabras guardadas en el diccionario
+    */
+    unsigned int size() const;
+
+    /**
+    * @brief Comprueba si el diccionario está vacio.
+    * @return true si el diccionario está vacío, false en caso contrario
+    */
+    bool empty() const { return words.size() == 0; }
+
+    /**
+    * @brief Indica si una palabra esta en el diccionario o no. Este método comprueba si una determinada palabra se encuentra o no en el dicccionario
+    * @param palabra: la palabra que se quiere buscar.
+    * @return Booleano indicando si la palabra existe o no en el diccionario
+    */
+    bool exists(const string &val);
+
+    /**
+    * @brief Elimina una palabra del diccionario
+    * @param val Palabra a borrar del diccionario
+    * @return Booleano que indica si la palabra se ha borrado del diccionario
+    */
+    bool erase(const string &val);
+
+    /**
+    * @brief Sobrecarga del operador de entrada. Permite leer las palabras de un fichero de texto e introducirlas en el diccionario
+    * @param is Flujo de entrada
+    * @param dic Diccionario a rellenar
+    * @return Flujo de entrada para poder encadenar el operador
+    */
+    friend istream &operator>>(istream &is, Dictionary &dic);
+
+    /**
+    * @brief Sobrecarga del operador de salida. Permite imprimir el diccionario completo a un flujo de salida
+    * @param os Flujo de salida, donde imprimir el diccionario
+    * @param dic Diccionario a imprimir
+    * @return Flujo de salida, para poder encadenar el operador
+    */
+    friend ostream &operator<<(ostream &os, const Dictionary &dic);
+    /**
+    * @brief Indica el numero de apariciones de una letra.
+    * @param c letra a buscar.
+    * @return Un entero indicando el numero de apariciones.
+    */
+    int getOcurrences(const char c) const;
+
+    /**
+    * @brief Indica el numero totales de letras
+    * @return Un entero indicando el numero totales de letras
+    */
+    int getTotalLetters() const;
+
+    /**
+    * @brief Obtiene todas las palabras de una longitud
+    * @param longitud: valor de longitud de la palabras a devolver
+    * @return un vector con palabra de la longitud dada
+    */
+    vector<string> getWordsLength(int longitud);
+
+    /**
+    * @brief Iterador del diccionario. Obtiene las palabras ordenadas alfabéticamente
+    */
     class iterator
     {
       private:
         set<string>::iterator it;
 
       public:
-        friend class Dictionary;
         iterator();
         bool operator==(const iterator &i) const;
         bool operator!=(const iterator &i) const;
         iterator &operator++();
         const string &operator*();
+        friend class Dictionary;
     };
 
+    /**
+    * @brief Iterador constante del diccionario. Obtiene las palabras ordenadas alfabéticamente
+    */
     class const_iterator
     {
       private:
         set<string>::const_iterator it;
 
       public:
-        friend class Dictionary;
         const_iterator();
         bool operator==(const const_iterator &i) const;
         bool operator!=(const const_iterator &i) const;
         const_iterator &operator++();
         const string &operator*() const;
+        friend class Dictionary;
     };
 
+    /**
+    * @brief Indica si una palabra esta en el diccionario
+    * @return iterador apuntando a la palabra si esta o end si no esta
+    */
     iterator find(const string &w);
+
+    /**
+    * @brief Obtiene todas las palabras de una longitud
+    * @param longitud: valor de longitud de la palabras a devolver
+    * @return un vector con palabra de la longitud dada
+    */
     pair<iterator, bool> insert(const string &val);
+
+    /**
+    * @brief Busca un las palabras con un prefijo
+    * @param val prefijo a buscar
+    * @return un pair con dos iteradores el primero apuntando a la primera palaba con el prefijo y el segundo donde ya no contiene el prefijo. Si no existe el prefijo se devuelve los dos iteradores apuntando a end()
+    */
     pair<iterator, iterator> range_prefix(const string &val);
+
+    /**
+    * @brief Obtiene el iterador apuntando a la primera palabra del diccionario
+    */
     iterator begin();
     const_iterator cbegin() const;
+
+    /**
+    * @brief Obtiene el iterador apuntando al final del diccionario.
+    */
     iterator end();
     const_iterator cend() const;
 };
--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -1,15 +1,5 @@
 #include "dictionary.h"
 
-Dictionary::Dictionary()
-{
-    this->words = set<string>();
-}
-
-Dictionary::~Dictionary()
-{
-    clear();
-}
-
 void Dictionary::clear()
 {
     this->words.clear();
@@ -20,11 +10,6 @@ unsigned int Dictionary::size() const
     return this->words.size();
 }
 
-bool Dictionary::empty() const
-{
-    return this->words.empty();
-}
-
 bool Dictionary::exists(const string &val)
 {
     return this->words.find(val) != this->words.end();
@@ -32,22 +17,70 @@ bool Dictionary::exists(const string &val)
 
 bool Dictionary::erase(const string &val)
 {
-    return this->words.erase(val);
+    return this->words.erase(val) > 0;
+}
+
+istream &operator>>(istream &is, Dictionary &dic)
+{
+    string word;
+    
+    while(is >> word) dic.insert(word);
+    
+    return is;
+}
+
+ostream &operator<<(ostream &os, const Dictionary &dic)
+{
+    for(Dictionary::const_iterator it = dic.cbegin(); it != dic.cend(); ++it)
+        os << *it << endl;
+    
+    return os;
 }
 
 int Dictionary::getOcurrences(const char c) const
 {
+    int ocurrences = 0;
+    string word;
 
+    for(Dictionary::const_iterator it = cbegin(); it != cend(); ++it)
+    {
+        word = *it;
+        for(int i = 0; i < word.size(); i++ )
+            if(word[i] == c) ocurrences++;
+    }
+
+    return ocurrences;
 }
 
 int Dictionary::getTotalLetters() const
 {
+    int total = 0;
+    string word;
 
+    for(Dictionary::const_iterator it = cbegin(); it != cend(); ++it)
+    {
+        word = *it;
+        int word_size = word.size();
+        total += word_size;
+    }
+
+    return total;
 }
 
-vector<string> Dictionary::getWordsLength(int length)
+vector<string> Dictionary::getWordsLength(int longitud)
 {
+    vector<string> words_length;
+    string word;
+    int word_length;
 
+    for(Dictionary::const_iterator it = cbegin(); it != cend(); ++it)
+    {
+        word = *it;
+        word_length = word.size();
+        if(word_length == longitud) words_length.push_back(word);
+    }
+
+    return words_length;
 }
 
 Dictionary::iterator::iterator() = default;
@@ -56,7 +89,6 @@ bool Dictionary::iterator::operator==(const iterator &i) const
 {
     return this->it == i.it;
 }
-
 
 bool Dictionary::iterator::operator!=(const iterator &i) const
 {
@@ -99,17 +131,43 @@ const string &Dictionary::const_iterator::operator*() const
 
 Dictionary::iterator Dictionary::find(const string &w)
 {
-
+    Dictionary::iterator it;
+    it.it = this->words.find(w);
+    return it;
 }
 
 pair<Dictionary::iterator, bool> Dictionary::insert(const string &val)
 {
+    pair<set<string>::iterator, bool> insercion = words.insert(val);
 
+    Dictionary::iterator it;
+    it.it = insercion.first;
+    
+    return make_pair(it, insercion.second);;
 }
 
 pair<Dictionary::iterator, Dictionary::iterator> Dictionary::range_prefix(const string &prefix)
 {
+    pair<Dictionary::iterator, Dictionary::iterator> range;
+    Dictionary::iterator it1, it2;
 
+    set<string>::iterator inf = words.lower_bound(prefix);
+
+    if(inf == words.end() || inf->compare(0, prefix.size(), prefix) != 0)
+    {
+        it1.it = words.end();
+        it2.it = words.end();
+    }
+
+    string sup = prefix;
+    sup.push_back(char(127));
+    
+    set<string>::iterator sup_it = words.upper_bound(sup);
+    it1.it = inf;
+    it2.it = sup_it;
+    
+
+    return make_pair(it1, it2);
 }
 
 Dictionary::iterator Dictionary::begin()
